0482648c035a6568b519d6ce84e5b026dec419b2
sskryu322 <sence0322@icloud.com>
2022-06-01 23:48:32 +0900

    新規登録ページの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,9 @@ class User < ApplicationRecord
   has_many :group_lists
   has_many :messages
 
-  with_options presence: true do
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :nickname, :department, :position
-  end
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :nickname, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
