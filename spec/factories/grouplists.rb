ba9720d15608bf795f6907d78f4284aa4c12f8bd
sskryu322 <sence0322@icloud.com>
2022-06-08 16:06:06 +0900

    テストコード実行

--- spec/factories/grouplists.rb
+++ spec/factories/grouplists.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :grouplist do
+    title { Faker::Commerce.product_name }
+    start_time { '2023-01-01 11:00:00' }
+    text { Faker::Lorem.sentence }
+    association :user
+    association :room
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
+
